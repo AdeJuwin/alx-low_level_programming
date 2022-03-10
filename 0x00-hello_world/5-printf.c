7604eba54e9d4bd8d549f8f2fe07d9f3cf38bae5
Adejuwin <olajuwonsunday01@gmail.com>
2022-03-10 12:13:57 -0800

    Printing using prinft func

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
-/**
- * main function needs to return an integer after execution
- * the return value has to be 0
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-		return (0);
+
+
+/**
+ *
+ * *main - function needs to return an integer after execution
+ *
+ * *Return: return 0 as success and 1 as fail
+ *
+ */
+
+
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
