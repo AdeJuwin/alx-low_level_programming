20404ca6e8a0991a93cf6fbc0d158afe419216fc
Adejuwin <olajuwonsunday01@gmail.com>
2022-03-18 12:45:44 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function prints if integer positive or negative
+ * Return: return 0 as success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
